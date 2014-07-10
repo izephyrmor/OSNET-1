60c77bfd77c8744cf2b829739cd28eb547240f22
gloriamaris <monique.dingding@gmail.com>
2014-07-10 16:32:36 +0800

    Test commit. File affected: login.php

--- application/views/user/login.php
+++ application/views/user/login.php
@@ -2,21 +2,25 @@
 <div class="container">
   <div class="row">
     <?php echo validation_errors(); ?>
-	
-	<form method="post" accept-charset="utf-8" action="<?php site_url(); ?>/user/login"  class="login-form col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center"  id="myform" />
-		<input type="text" name="username" palceholder="Username" value="" required/>
-		<input type="password" name="password" placeholder="Username" value="" required/>
+  
+	<form method="post" accept-charset="utf-8" action="<?php echo site_url(); ?>home/login_validation"  class="login-form col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center" />
+		<h1></h1>
+		
+		<?php if(isset($login_error_message)): ?>
+			<div class="alert alert-danger alert-dismissible" role="alert">
+				<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+				<?php  echo $login_error_message; ?>
+			</div>
+		<?php endif; ?>
+		
+		<input type="text" name="username" placeholder="Username" value="" class="form-control" placeholder="Username" autofocus autocomplete="off" required/>
+		<input type="password" name="password" placeholder="Password" value="" placeholder="Password" class="form-control" required/>
 		<input type="submit" name="submit" value="Login" class="btn btn-lg btn-primary btn-block"/>
 	</form>
-	jsfhk
-    <?php echo form_open(site_url(), $form_attributes);?>
-        <?php echo form_input('username', '', 'class="form-control" placeholder="Username" autofocus'); ?>
-        <?php echo form_password('password', '', 'required placeholder="Password" class="form-control"'); ?>
-        <?php echo form_submit('submit', 'Login', 'class="btn btn-lg btn-primary btn-block"'); ?>
-    <?php echo form_close();?>
+	
   </div>
   <div class="row">
-    <p class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center copyright">&copy; <?php echo date('Y'); ?> OSnet. All Rights Reserved.</p>
+    <p class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center copyright">&copy; <?php echo date('Y'); ?> OS Net. All Rights Reserved.</p>
   </div>
 </div>
 <?php $this->load->view('templates/footer'); ?>
