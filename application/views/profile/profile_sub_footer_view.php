2573fd0cbd42b8faff1ecfbb9314bea93a3d2a0f
izephyrmor <izephyrmor@gmail.com>
2014-07-14 19:53:58 +0800

    Added Profile, My Account and Departmen People under Profile Nav Tab.

--- application/views/profile/profile_sub_footer_view.php
+++ application/views/profile/profile_sub_footer_view.php
@@ -68,35 +68,6 @@
 
 </section><!-- /.content -->
 
-  <!--
-  <div class="row">
-
-    <div class="col-md-9">
-
-      <div class="panel panel-primary">
-        <div class="panel-heading"></div>
-
-        <div class="panel-body">
-          louise
-        </div>
-      </div>
-
-    </div>
-
-    <div class="col-md-3">
-
-      <div class="panel panel-primary">
-        <div class="panel-heading"></div>
-
-        <div class="panel-body">
-          louise
-        </div>
-      </div>
-
-    </div>
-
-  </div>-->
-
 </aside>
 
 </div>
